8e67fa539670acf1fdc2d9f292742ab549778755
Yottabyte090 <yottabyte090@naver.com>
2018-02-18 22:09:47 +0900

    Initial commit

---
+++ ClickWar2 Password Recovery/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace ClickWar2_Password_Recovery
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ClickWar2 Password Recovery 1.0.0\r\nCopyright 2017 Yottabyte090. All rights reserv" +
+    "ed";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "당신의 비밀번호는...";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(245, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(263, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "입니다.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(297, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "클립보드에 복사";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 115);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ClickWar2 Password Recovery";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ClickWar2 Password Recovery/Form1.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ClickWar2_Password_Recovery
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (RegistryKey registry = Registry.CurrentUser.OpenSubKey("ClickWar2"))
+                {
+                    if (registry == null)
+                    {
+                        MessageBox.Show("ClickWar2 계정 정보 저장 이력이 없습니다.", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Application.Exit();
+                    }
+                    else
+                    {
+                        if(registry.GetValue("LoginPass") == null || registry.GetValue("LoginKey") == null)
+                        {
+                            MessageBox.Show("LoginPass 또는 LoginKey의 값이 null입니다.", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            Application.Exit();
+                        }
+                        else
+                        {
+                            string data = (string) registry.GetValue("LoginPass");
+                            string key = (string) registry.GetValue("LoginKey");
+
+                            RijndaelManaged aes = new RijndaelManaged();
+                            aes.KeySize = 256;
+                            aes.BlockSize = 128;
+                            aes.Mode = CipherMode.CBC;
+                            aes.Padding = PaddingMode.PKCS7;
+                            aes.Key = Enumerable.Range(0, key.Length)
+                                .Where(x => x % 2 == 0)
+                                .Select(x => Convert.ToByte(key.Substring(x, 2), 16))
+                                .ToArray();
+                            aes.IV = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+                            var decryptor = aes.CreateDecryptor();
+                            byte[] buffer = null;
+                            using(var mStream = new MemoryStream())
+                            {
+                                using (var cStream = new CryptoStream(mStream, decryptor, CryptoStreamMode.Write))
+                                {
+                                    byte[] xStr = Convert.FromBase64String(data);
+                                    cStream.Write(xStr, 0, xStr.Length);
+                                }
+
+                                buffer = mStream.ToArray();
+                            }
+
+                            textBox1.Text = Encoding.UTF8.GetString(buffer);
+                        }
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString(), "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox1.Text);
+        }
+    }
+}
